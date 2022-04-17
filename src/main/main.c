09e65c07b008b1b9910a5ce926eb8d12e16327cd
Jeffrey Price <jeff.price@opswat.com>
2022-04-16 20:20:34 -0400

    cleanup main program

--- src/main/main.c
+++ src/main/main.c
@@ -6,16 +6,15 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#include "../shared/log.h"
+#include "../shared/worker_thread_pool.h"
+
 #define DEFAULT_PORT 8000
 #define DEFAULT_WORKER_POOL_SIZE 2
 
-#define ONE_SECOND_IN_MICROSECONDS 1000000
-
-struct worker_thread_data {
-	pthread_t thread;
-	int id;
-	int running;
-};
+typedef struct {
+	int unused;
+} http_worker_task_data;
 
 void usage(char *name) {
 	printf("usage:\n");
@@ -26,14 +25,10 @@ void usage(char *name) {
 	printf("        Listen on this port\n");
 }
 
-void *worker_thread(void *data) {
-	struct worker_thread_data *thread_data = data;
-	printf("TODO implement worker thread %i\n", thread_data->id);
-	while (thread_data->running) {
-		printf("TODO JEFF worker thread tick %i\n", thread_data->id);
-		usleep(ONE_SECOND_IN_MICROSECONDS);
-	}
-	return NULL;
+int http_worker_task(void *data) {
+	http_worker_task_data *d = data;
+	log_debug("TODO JEFF http_worker_task\n");
+	return 0;
 }
 
 int main(int argc, char **argv) {
@@ -60,46 +55,32 @@ int main(int argc, char **argv) {
 				exit(1);
 			}
 			if (sscanf(optarg, "%i", &port) != 1) {
-				fprintf(stderr, "failed to parse port: %s\n", optarg);
+				log_error("failed to parse port: %s\n", optarg);
 				exit(1);
 			}
 			continue;
 		}
-		fprintf(stderr, "unrecognized arg: %s\n", argv[optind - 1]);
+		log_error("unrecognized arg: %s\n", argv[optind - 1]);
 		usage(argv[0]);
 		exit(1);
 		break;
 	}
 	if (optind < argc) {
-		fprintf(stderr, "unrecognized arg: %s\n", argv[optind]);
+		log_error("unrecognized arg: %s\n", argv[optind]);
 		usage(argv[0]);
 		exit(1);
 	}
 
-	int socket_handle = socket(AF_INET, SOCK_STREAM, 0);
-	if (!socket_handle) {
-		fprintf(stderr, "error creating socket\n");
-		return 1;
-	}
-
-	int worker_pool_size = DEFAULT_WORKER_POOL_SIZE;
-
 	int return_value = 0;
 
-	// start worker thread pool
-	struct worker_thread_data *worker_threads = calloc(worker_pool_size, sizeof(struct worker_thread_data));
-	for (int i = 0; i < worker_pool_size; i++) {
-		worker_threads[i].id = i;
-		worker_threads[i].running = 1;
-		if (pthread_create(&worker_threads[i].thread, NULL, worker_thread, &worker_threads[i])) {
-			// TODO JEFF log the return value of pthread_create, it's an error code
-			fprintf(stderr, "failed to start worker threads\n");
-			return_value = 1;
-			goto DONE;
-		}
+	int socket_handle = socket(AF_INET, SOCK_STREAM, 0);
+	int socket_needs_close = 0;
+	if (!socket_handle) {
+		log_error("error creating socket\n");
+		return_value = 1;
+		goto DONE;
 	}
-
-	// listen on socket
+	socket_needs_close = 1;
 
 	struct sockaddr_in address;
 	address.sin_family = AF_INET;
@@ -107,7 +88,7 @@ int main(int argc, char **argv) {
 	address.sin_port = htons(port);
 	if (bind(socket_handle, (struct sockaddr *)&address, sizeof(address)) < 0) {
 		// TODO check errno
-		fprintf(stderr, "failed to bind socket to port %i\n", port);
+		log_error("failed to bind socket to port %i\n", port);
 		return_value = 1;
 		goto DONE;
 	}
@@ -115,25 +96,27 @@ int main(int argc, char **argv) {
 	// TODO 2nd arg is how many pending connections should be allowed to be queued here, what's a good number?
 	if (listen(socket_handle, 0) < 0) {
 		// TODO check errno
-		fprintf(stderr, "failed to listen on socket");
-		exit(1);
+		log_error("failed to listen on socket\n");
 		return_value = 1;
 		goto DONE;
 	}
 
-	printf("TODO listen\n");
+	// start worker thread pool
+	worker_thread_pool http_worker_pool;
+	int http_worker_pool_needs_destroy = 0;
+	if (worker_thread_pool_init(&http_worker_pool, 1, 10)) {
+		log_error("failed to make thread pool\n");
+		return_value = 1;
+		goto DONE;
+	}
+	http_worker_pool_needs_destroy = 1;
+
+	log_debug("TODO JEFF listen to socket\n");
 
 DONE:
-	for (int i = 0; i < worker_pool_size; i++) {
-		worker_threads[i].running = 0;
-		// TODO signal any locks or semaphores so thread can abort what it's doing or wake up
+	if (http_worker_pool_needs_destroy) {
+		worker_thread_pool_destroy(&http_worker_pool);
 	}
-	for (int i = 0; i < worker_pool_size; i++) {
-		if (pthread_join(worker_threads[i].thread, NULL)) {
-			fprintf(stderr, "error occurred waiting on worker thread\n");
-		}
-	}
-	free(worker_threads);
 	close(socket_handle);
 	return return_value;
 }
