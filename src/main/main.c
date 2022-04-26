cf98b2a356fefb5f9a7bcf8951ca0680a6edbdad
Jeffrey Price <jeff.price@opswat.com>
2022-04-26 19:59:25 -0400

    .

--- src/main/main.c
+++ src/main/main.c
@@ -33,15 +33,21 @@ typedef struct {
 	http_header *headers;
 } http_headers;
 
+typedef struct {
+	char *method;
+	char *uri;
+	http_headers headers;
+} http_request;
+
 /**
  * Initialize the given header struct with the given name. The values are initialized to an empty list.
  *
  * The length of the name is given by nameLen (not including a terminating 0).
  */
-void http_header_init(http_header *header, char *name, size_t nameLen) {
-	header->name = malloc(nameLen + 1);
-	memcpy(header->name, name, nameLen);
-	header->name[nameLen] = 0;
+void http_header_init(http_header *header, char *name, size_t name_len) {
+	header->name = malloc(name_len + 1);
+	memcpy(header->name, name, name_len);
+	header->name[name_len] = 0;
 	header->values_len = 0;
 	header->values = NULL;
 }
@@ -51,10 +57,10 @@ void http_header_init(http_header *header, char *name, size_t nameLen) {
  *
  * The length of the values string is given by valuesLen (not including a terminating 0).
  */
-void http_header_append(http_header *header, char *values, size_t valuesLen) {
+void http_header_append(http_header *header, char *values, size_t values_len) {
 	size_t last = 0;
 	size_t i;
-	for (i = 0; i < valuesLen; i++) {
+	for (i = 0; i < values_len; i++) {
 		if (values[i] == ',') {
 			header->values_len++;
 			header->values = realloc(header->values, sizeof(char *) * header->values_len);
@@ -104,17 +110,33 @@ void http_headers_dealloc(http_headers *headers) {
 	}
 }
 
-http_header *http_headers_get_or_create(http_headers *headers, char *name, size_t nameLen) {
+http_header *http_headers_get_or_create(http_headers *headers, char *name, size_t name_len) {
 	for (size_t i = 0; i < headers->len; i++) {
 		size_t len = strlen(headers->headers[i].name);
-		if (nameLen == len && !memcmp(headers->headers[i].name, name, len)) {
+		if (name_len == len && !memcmp(headers->headers[i].name, name, len)) {
 			return &headers->headers[i];
 		}
 	}
 	headers->len++;
 	headers->headers = realloc(headers->headers, sizeof(http_header) * headers->len);
-	http_header_init(&headers->headers[headers->len - 1], name, nameLen);
+	http_header_init(&headers->headers[headers->len - 1], name, name_len);
 	return &headers->headers[headers->len - 1];
+}
+
+void http_request_init(http_request *request, char *method, size_t method_len, char *uri, size_t uri_len) {
+	request->method = malloc(method_len + 1);
+	memcpy(request->method, method, method_len);
+	request->method[method_len] = 0;
+	request->uri = malloc(uri_len + 1);
+	memcpy(request->uri, uri, uri_len);
+	request->uri[uri_len] = 0;
+	http_headers_init(&request->headers);
+}
+
+void http_request_dealloc(http_request *request) {
+	free(request->method);
+	free(request->uri);
+	http_headers_dealloc(&request->headers);
 }
 
 int shutdown_requested;
@@ -149,13 +171,16 @@ int http_task(int id, void *d) {
 
 	size_t end_of_last_line = 0;
 	int found_request_line = 0;
+	size_t end_of_request_line;
 	int found_end_of_header = 0;
-	http_headers headers;
-	http_headers_init(&headers);
 	size_t start_of_body;
 
+	int request_init = 0;
+	http_request request;
+
 	// use max size -1 because we're going to want to put a terminating 0
-	while (buffer_length < MAX_SOCKET_READ_SIZE_IN_CHUNKS * CHUNK_READ_SIZE - 1) {
+	size_t max_read_length = MAX_SOCKET_READ_SIZE_IN_CHUNKS * CHUNK_READ_SIZE;
+	while (buffer_length < max_read_length) {
 		size_t result = read(data->socket, buffer + buffer_length, buffer_capacity - buffer_length);
 		if (result < 0) {
 			log_error("http_task failed, error reading from socket %s\n", strerror(errno));
@@ -177,36 +202,108 @@ int http_task(int id, void *d) {
 			for (size_t i = end_of_last_line; i < buffer_length - 1; i++) {
 				// rfc says all lines before the body start with CRLF
 				if (buffer[i] == '\r' && buffer[i + 1] == '\n') {
-					size_t lineLength = i - end_of_last_line;
-					// TODO JEFF line is for easy debugging
-					char *line = malloc(lineLength + 1);
-					memcpy(line, buffer + end_of_last_line, lineLength);
-					line[lineLength] = 0;
+					size_t line_length = i - end_of_last_line;
 					if (!found_request_line) {
 						found_request_line = 1;
-						printf("TODO JEFF parse request line: %s\n", line);
-					} else if (!found_end_of_header) {
-						if (lineLength == 0) {
+						end_of_request_line = i;
+						int found_start_of_method = 0;
+						int found_end_of_method = 0;
+						size_t method_start, method_end;
+						int found_start_of_uri = 0;
+						int found_end_of_uri = 0;
+						size_t uri_start, uri_end;
+						int found_start_of_protocol_version = 0;
+						int found_end_of_protocol_version = 0;
+						size_t protocol_version_start, protocol_version_end;
+						for (size_t i = 0; i < end_of_request_line; i++) {
+							int is_space = buffer[i] == ' ';
+							if (!found_start_of_method) {
+								if (!is_space) {
+									found_start_of_method = 1;
+									method_start = i;
+								}
+							} else if (!found_end_of_method) {
+								if (is_space) {
+									found_end_of_method = 1;
+									method_end = i;
+								}
+							} else if (!found_start_of_uri) {
+								if (!is_space) {
+									found_start_of_uri = 1;
+									uri_start = i;
+								}
+							} else if (!found_end_of_uri) {
+								if (is_space) {
+									found_end_of_uri = 1;
+									uri_end = i;
+								}
+							} else if (!found_start_of_protocol_version) {
+								if (!is_space) {
+									found_start_of_protocol_version = 1;
+									protocol_version_start = i;
+								}
+							} else if (!found_end_of_protocol_version) {
+								if (is_space) {
+									found_end_of_protocol_version = 1;
+									protocol_version_end = i;
+									break;
+								} else if (i == end_of_request_line - 1) {
+									found_end_of_protocol_version = 1;
+									protocol_version_end = i + 1;
+									break;
+								}
+							}
+						}
+						if (found_end_of_protocol_version) {
+							request_init = 1;
+							http_request_init(&request, buffer + method_start, method_end - method_start, buffer + uri_start,
+											  uri_end - uri_start);
+						} else {
+							char *request_line = malloc(end_of_request_line + 1);
+							memcpy(request_line, buffer, end_of_request_line);
+							request_line[end_of_request_line] = 0;
+							log_trace("TODO JEFF failed to parse request line: %s\n", request_line);
+							free(request_line);
+						}
+					} else if (request_init && !found_end_of_header) {
+						if (line_length == 0) {
 							found_end_of_header = 1;
 							start_of_body = i + 2;
+							// at this point we either have a content length header or we don't
+							// either way we know exactly how many more bytes to read, which may be 0
+							http_header *header = http_headers_get_or_create(&request.headers, "Content-Length", 14);
+							if (header->values_len == 1) {
+								int content_length;
+								if (sscanf(header->values[0], "%i", &content_length) == 1) {
+									max_read_length = buffer_length + content_length;
+								} else {
+									log_error("TODO JEFF handle error with bad content-length: %s\n", header->values[0]);
+								}
+							} else {
+								max_read_length = buffer_length;
+							}
+							break;
 						} else {
 							size_t colonIndex = 0;
 							int foundSeparator = 0;
-							for (colonIndex = 0; colonIndex < lineLength; colonIndex++) {
+							for (colonIndex = 0; colonIndex < line_length; colonIndex++) {
 								if (buffer[end_of_last_line + colonIndex] == ':') {
 									foundSeparator = 1;
 									break;
 								}
 							}
 							if (foundSeparator) {
-								http_header *header = http_headers_get_or_create(&headers, buffer + end_of_last_line, colonIndex);
-								http_header_append(header, buffer + end_of_last_line + colonIndex + 1, lineLength - colonIndex - 1);
+								http_header *header = http_headers_get_or_create(&request.headers, buffer + end_of_last_line, colonIndex);
+								http_header_append(header, buffer + end_of_last_line + colonIndex + 1, line_length - colonIndex - 1);
 							} else {
+								char *line = malloc(line_length + 1);
+								memcpy(line, buffer + end_of_last_line, line_length);
+								line[line_length] = 0;
 								log_error("TODO JEFF handle error parsing header: %s\n", line);
+								free(line);
 							}
 						}
 					}
-					free(line);
 					// skip the newline
 					end_of_last_line = i + 2;
 					i++;
@@ -216,29 +313,36 @@ int http_task(int id, void *d) {
 	}
 	buffer[buffer_length] = 0;
 
-	log_trace("TODO JEFF headers len: %lu\n", headers.len);
-	for (size_t i = 0; i < headers.len; i++) {
-		log_trace("TODO JEFF header[%lu] = %s\n", i, headers.headers[i].name);
-		for (size_t j = 0; j < headers.headers[i].values_len; j++) {
-			log_trace("TODO JEFF     value[%lu] = %s\n", j, headers.headers[i].values[j]);
+	if (request_init) {
+		log_trace("TODO JEFF method: %s\n", request.method);
+		log_trace("TODO JEFF uri: %s\n", request.uri);
+
+		log_trace("TODO JEFF headers len: %lu\n", request.headers.len);
+		for (size_t i = 0; i < request.headers.len; i++) {
+			log_trace("TODO JEFF header[%lu] = %s\n", i, request.headers.headers[i].name);
+			for (size_t j = 0; j < request.headers.headers[i].values_len; j++) {
+				log_trace("TODO JEFF     value[%lu] = %s\n", j, request.headers.headers[i].values[j]);
+			}
 		}
+
+		size_t body_length = buffer_length - start_of_body;
+		if (body_length > 0) {
+			// TODO body is for debugging
+			char *body = malloc(body_length + 1);
+			memcpy(body, buffer + start_of_body, body_length);
+			body[body_length] = 0;
+			log_trace("TODO JEFF body len: %lu\nbody:\n%s\n", body_length, body);
+			free(body);
+		} else {
+			log_trace("TODO JEFF no body\n");
+		}
+
+		http_request_dealloc(&request);
 	}
 
-	size_t body_length = buffer_length - start_of_body;
-	if (body_length > 0) {
-		// TODO body is for debugging
-		char *body = malloc(body_length + 1);
-		memcpy(body, buffer + start_of_body, body_length);
-		body[body_length] = 0;
-		log_trace("TODO JEFF body len: %lu\nbody:\n%s\n", body_length, body);
-		free(body);
-	} else {
-		log_trace("TODO JEFF no body\n");
-	}
-
-	http_headers_dealloc(&headers);
 	free(buffer);
 	close(data->socket);
+	free(data);
 	return 0;
 }
 
